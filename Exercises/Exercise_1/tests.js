36d3dcf74f11ab646245572d8ca992fc2ef429f6
Stephan Ariesanu <stephan.aran@gmail.com>
2017-05-22 00:26:34 +0200

    created directory structure and added first exercise

---
+++ Exercises/Exercise_1/tests.js
@@ -0,0 +1,6 @@
+if(hello()=="Hello World!"){
+  succes("Your function is correct");
+}
+else{
+  failure("There is something wrong. We explected: 'Hello World!', but instead we got:'"+hello()+"'");
+}
